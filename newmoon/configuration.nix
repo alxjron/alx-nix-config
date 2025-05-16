0ccb2fc0d8c1dc87b783e137bd738cde5849a4e4
= <alxjron@protonmail.com>
2025-05-16 11:43:44 -0700

    Replace systemdboot with grub

--- newmoon/configuration.nix
+++ newmoon/configuration.nix
@@ -16,8 +16,11 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.grub.enable = true; 
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
 
   networking.hostName = "newmoon"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
